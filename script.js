30db9bf378f0be29fcb1a1397fe7d2d48c36a9e6
maatneter <95445696+maatneter@users.noreply.github.com>
2022-03-09 08:11:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function compute()
     }
     let interest = principal.value * years.value * rate.value / 100;
     yearInFuture = new Date().getFullYear() + parseInt(years.value);
-    result = document.getElementById("result")
-    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
+    results = document.getElementById("results")
+    results.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
 
 }
 
