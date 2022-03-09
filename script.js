79aa7ba7379eef7b01e84e72d873451b06222327
maatneter <95445696+maatneter@users.noreply.github.com>
2022-03-09 08:02:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+function compute()
+{
+    if(principal.value < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
+    }
+    let interest = principal.value * years.value * rate.value / 100;
+    yearInFuture = new Date().getFullYear() + parseInt(years.value);
+    result = document.getElementById("result")
+    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+yearInFuture+"</mark></p>";
 
-    if (principal <= 0) {
-        window.alert("Enter a positive number");
-        document.getElementById("principal").focus();
-    } else {
-        var interest = principal * rate * years / 100;
-        var yearNow = new Date().getFullYear();
-        var yearsFromNow = (+yearNow) + (+years);
-        var result = document.getElementById("result");
-        result.innerHTML =
-            '<div>' +
-            'If you deposit <span class="number" id="principalResult\">' + principal + '</span>,<br/>' +
-            'at an interest rate of <span class="number" id="rateResult">' + rate + '%</span>.<br/>' +
-            'You will receive an amount of <span class="number" id="interestResult">' + interest + '</span>,<br/>' +
-            'in the year <span class="number" id="futureYearResult">' + yearsFromNow + '</span>.<br/>' +
-            '</div>'
+}
+
+function refreshSlider(){
+rate.value
+document.getElementById("rateLabel").innerText = rate.value + "%"}
+
+finishPage = () =>{
+    var rate = document.getElementById("rate");
+    var principal = document.getElementById("principal");
+    var years = document.getElementById("years");
+    years = document.getElementById("years");
+    for(i=1; i<=10; i++){
+    	let element = document.createElement("option")
+    	element.innerText = i
+    	element.setAttribute("value", i)
+    	years.appendChild(element)
     }
 }
 
-function slider() { 
-    var slideValue = document.getElementById("rate").value;
-    document.getElementById("rateLabel").innerHTML = refreshslider + "%";
-}
 
